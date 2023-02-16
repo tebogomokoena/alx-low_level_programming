17dbcc90425f6642ae34a4e00de55f3fcc476689
tebogo mokoena <mariomokoena10@gmail.com>
2023-02-16 02:15:56 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <std.h>
 /**
  * main - A program that prints a line with puts function
- * Return: 0
+ * Return: 0 (Success)
  */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
